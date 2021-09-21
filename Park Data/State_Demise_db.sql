35803333a341ab4c185243749934a5d0271b37e7
Alexander Espinosa <alexander.edward.espinosa@gmail.com>
2021-09-20 21:58:11 -0500

    ALX FIX

--- Park Data/State_Demise_db.sql
+++ Park Data/State_Demise_db.sql
@@ -2,7 +2,8 @@ drop table if exists Park_Deaths_State;
 
 create table Park_Deaths_State (
 	state char(2),
-	deaths int
+	deaths int,
+	Park_Name char(50)
 );
 
-select * from PArk_Deaths_State
+select * from Park_Deaths_State
